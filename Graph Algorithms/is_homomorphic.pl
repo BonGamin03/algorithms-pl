6c34f9c306c710f7ebf48d3a01a48c50520757f3
Amaranto <danielmares2003@gmail.com>
2025-12-21 01:32:48 -0500

    Add predicate to check the homomorphicsConditions

--- Graph Algorithms/is_homomorphic.pl
+++ Graph Algorithms/is_homomorphic.pl
@@ -19,3 +19,14 @@ edge2(3, 4).
 edge2(3, 5).
 edge2(4, 6).
 edge2(5, 6).
+
+% The predicate to check if two graphs are homomorphic
+
+is_homomorphic :-
+    nodes1(Nodes1),                                                                         % Get the nodes of both graphs.
+    nodes2(Nodes2),
+    length(Nodes1, L),                                                                      % Ensure both graphs have the same number of nodes.
+    length(Nodes2, L),
+    findall((Node1, Node2), (member(Node1, Nodes1), member(Node2, Nodes2)), Pairs),         % Find all possible pairs of nodes.
+    permutation(Pairs, Permutation),                                                        % Check permutations of these pairs.
+    check_homomorphism(Permutation). 
