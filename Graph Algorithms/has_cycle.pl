6d78249cb16abc9056f83fd73b257dd40e0e316e
Amaranto <danielmares2003@gmail.com>
2025-12-21 15:16:26 -0500

    Add cycle predicate

--- Graph Algorithms/has_cycle.pl
+++ Graph Algorithms/has_cycle.pl
@@ -16,3 +16,15 @@ edge(f, c).
 has_cycle :-
     edge(X, _),                                                 % Start from each node with an outgoing edge.
     cycle(X, [X]).                                              % Check for cycles starting from node X.
+
+
+% Base case: A cycle is detected if a node revisits any node in the visited list.
+cycle(X, Visited) :-
+    edge(X, Z),
+    member(Z, Visited).
+
+% Recursive case: Move to the next node and continue checking for cycles.
+cycle(X, Visited) :-
+    edge(X, Z),
+    \+ member(Z, Visited),                                      % Ensure no node is revisited.
+    cycle(Z, [Z|Visited]).
