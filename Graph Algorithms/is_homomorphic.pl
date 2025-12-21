93675f65ef45895e9cf54b74744546d6e03db612
Amaranto <danielmares2003@gmail.com>
2025-12-21 00:21:13 -0500

    Define homomorphic in graph

---
+++ Graph Algorithms/is_homomorphic.pl
@@ -0,0 +1,21 @@
+
+% Check Homomorphism Between Two Graphs
+% Define edges of the first graph
+
+edge1(a, b).
+edge1(a, c).
+edge1(b, d).
+edge1(c, d).
+edge1(c, e).
+edge1(d, f).
+edge1(e, f).
+
+% Define edges of the second graph
+
+edge2(1, 2).
+edge2(1, 3).
+edge2(2, 4).
+edge2(3, 4).
+edge2(3, 5).
+edge2(4, 6).
+edge2(5, 6).
